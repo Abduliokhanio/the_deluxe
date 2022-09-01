ff66340ea89b2f81472b7a358b96020256940711
abduliokhanio <akhan17@outlook.com>
2022-09-01 15:23:23 -0500

    added spec for routing

--- spec/acceptance/landing_page_spec.rb
+++ spec/acceptance/landing_page_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'Landing Page' do
+RSpec.describe 'Landing Page', type: :routing  do
   describe 'when a user navigates to root' do
-    xit 'renders home#index' do
+    it 'renders home#index' do
       # we don't have a default root path yet
+      expect(get("/")).to route_to("home#index")
     end
   end
 end
