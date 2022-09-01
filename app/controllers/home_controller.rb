47e470e3b57b30b4eb0daad9a9261c4eec053236
abduliokhanio <akhan17@outlook.com>
2022-09-01 15:45:16 -0500

    updated event to only have current and future even

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all
+    present = Date.today # to highlight that we're in the present
+    @events = Event.all.where('event_time >= ?', present)
   end
 end
