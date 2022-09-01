e6e131877efafaf941bb3f4bbeed2a4eb86a96de
abduliokhanio <akhan17@outlook.com>
2022-09-01 16:18:35 -0500

    removed pry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
     present = DateTime.now # to highlight that we're in the present
-    binding.pry
     @events = Event.all.where('event_time >= ?', present)
   end
 end
