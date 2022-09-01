b965a811fba9c80aa8049f73a5d4b6dd1cc21849
abduliokhanio <akhan17@outlook.com>
2022-09-01 14:34:06 -0500

    got root route set up

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :admins
   resources :events
   resources :home, only: [ :index ]
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
