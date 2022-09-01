51347e13a92b4a2e428867b11e600e245e089632
abduliokhanio <akhan17@outlook.com>
2022-09-01 16:18:00 -0500

    updated event to present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    present = Date.today # to highlight that we're in the present
+    present = DateTime.now # to highlight that we're in the present
+    binding.pry
     @events = Event.all.where('event_time >= ?', present)
   end
 end
